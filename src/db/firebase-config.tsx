37001f3cb890ce86ea6fa0cd451e7c7a7878d9e8
RaelEstate <rhyllneri@gmail.com>
2024-02-13 12:11:39 +0800

    wewe

--- src/db/firebase-config.tsx
+++ src/db/firebase-config.tsx
@@ -3,13 +3,21 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDlyzZB9fH4W2swddDynGTIuMJfhC4VxKg",
+  authDomain: "freedomboard-29042.firebaseapp.com",
+  projectId: "freedomboard-29042",
+  storageBucket: "freedomboard-29042.appspot.com",
+  messagingSenderId: "481646829567",
+  appId: "1:481646829567:web:56874475f9ed64ba61d1ca",
+  measurementId: "G-YVMBK91FS6",
+
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
