5b439a380ef36c3b1ec7f389ea84e9d6da47ba05
RaelEstate <rhyllneri@gmail.com>
2024-02-14 22:10:32 +0800

    34

--- src/main.js
+++ src/main.js
@@ -1,11 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
+// Create a div element to serve as the root container
+const rootElement = document.createElement("div");
+rootElement.id = "root";
+document.body.appendChild(rootElement);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Render the app inside the root container
+function render() {
+  rootElement.innerHTML = `
+    <div>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+      <div id="root-inner"></div>
+    </div>
+  `;
+  // Render the app component directly using vanilla JS
+  const appElement = React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(App, null)
+  );
+  const rootInnerElement = document.getElementById("root-inner");
+  ReactDOM.render(appElement, rootInnerElement);
+}
+
+render();
+
+// Load Bootstrap JS without using type="module"
+const scriptElement = document.createElement("script");
+scriptElement.src =
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js";
+document.body.appendChild(scriptElement);
