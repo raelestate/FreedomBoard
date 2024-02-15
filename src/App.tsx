8fc9d2ed75739b85ba8929b252ae5d24e5a45c16
RaelEstate <rhyllneri@gmail.com>
2024-02-15 14:38:39 +0800

    213423423

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FreedomBoard from "./pages/FreedomBoard";
 import PublicBoard from "./pages/PublicBoard";
 import UserListPage from "./pages/UserListPage";
-// import express from "express";
-// import path from "path";
-
-// const app = express();
-
-// // Middleware to set MIME type for JavaScript files
-// app.use((req: any, res: any, next: any): void => {
-//   if (req.url.endsWith(".js")) {
-//     res.setHeader("Content-Type", "application/javascript");
-//   }
-//   next();
-// });
-
-// // Serve static files
-// app.use(express.static(path.join(__dirname, "public")));
-
-// // Define your routes and other middleware here...
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
 function App() {
   return (
     <BrowserRouter>
--- src/components/message.js
+++
@@ -1,9 +0,0 @@
-const Message = ({ msg }) => {
-  return (
-    <div className="container">
-      <h6 className="display-6 text-center p-5 pt-5 pb-5">{msg}</h6>
-    </div>
-  );
-};
-
-export default Message;
