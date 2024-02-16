ec6f0dd339c4566190656b04d149d819d78d8c88
RaelEstate <rhyllneri@gmail.com>
2024-02-17 01:41:19 +0800

    we

--- src/db/db.js
+++ src/db/db.js
@@ -1,23 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDlyzZB9fH4W2swddDynGTIuMJfhC4VxKg",
-  authDomain: "freedomboard-29042.firebaseapp.com",
-  projectId: "freedomboard-29042",
-  storageBucket: "freedomboard-29042.appspot.com",
-  messagingSenderId: "481646829567",
-  appId: "1:481646829567:web:56874475f9ed64ba61d1ca",
-  measurementId: "G-YVMBK91FS6",
-
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
